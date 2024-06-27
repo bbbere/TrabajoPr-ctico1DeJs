a1de79452f32e22ad7424f2202415b03abded16a
Berenice Contreras <contbere@gmail.com>
2024-06-27 16:42:35 -0300

    Intenté hacer el ejercicio 4 que consiste en hacer un prompt que pida tu nombre y arroje un saludo con el nombre

---
+++ js/04-ejercicio.js
@@ -0,0 +1,8 @@
+/* 4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+Ejemplo: 
+input: Coder 
+Output: Hola Coder*/
+
+const NombreDeUsuario = prompt ("¿Cuál es tu nombre?");
+console.log(NombreDeUsuario)
+const resultado = "Hola" + NombreDeUsuario
