1749bec70996c0a005227695ee5821dca59b5eaf
Berenice Contreras <contbere@gmail.com>
2024-07-01 08:43:15 -0300

    Realicé el ejercicio 5, que consiste en pedir dos números y que de el resultado de estos dos

--- js/05-ejercicio.js
+++ js/05-ejercicio.js
@@ -1,1 +1,4 @@
 // Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+const numero1 = parseInt (prompt + ("Ingresa el primer número"))
+const numero2 = parseInt (prompt + ("Ingresa el segundo número"))
+alert ("El resultado es:" + (numero1 + numero2))
