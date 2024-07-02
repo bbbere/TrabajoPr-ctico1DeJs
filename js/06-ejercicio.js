8775def2efc41ea317180ff86b3f912d1888175e
Berenice Contreras <contbere@gmail.com>
2024-07-02 14:45:28 -0300

    ejercicio6 realizado. Programa que muestra el mayor de los dos numeros ingresados

--- js/06-ejercicio.js
+++ js/06-ejercicio.js
@@ -1,1 +1,11 @@
 // Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+const numero1 = parseInt(prompt ('Ingresa un número'))
+const numero2 = parseInt(prompt ('Ingresa otro número'))
+
+if (numero1 > numero2) {
+    document.write(`${numero1} es el mayor de los dos números ingresados`);
+} else if (numero2 > numero1) {
+    document.write(`${numero2} es el mayor de los dos números ingresados`);
+} else {
+    document.write('Ambos números son iguales');
+}
